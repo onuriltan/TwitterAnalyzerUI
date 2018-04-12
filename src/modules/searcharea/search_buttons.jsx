c8acad9bf5e5fbb831e07090651c739e330a9386
onur.iltan <onur.iltan@altpro.com.tr>
2018-04-12 17:30:25 +0300

    start button refreshes the whole state, but doesnt change map, need to be refactored later

--- src/modules/searcharea/search_buttons.jsx
+++ src/modules/searcharea/search_buttons.jsx
@@ -72,21 +72,27 @@ class SearcButtons extends Component {
 
         this.setState({ start_disabled: true });
         this.setState({ stop_disabled: false });
-        if (this.props.state.reducer.initialload === false) {
             var payload = {
                 data: {
+                    csrf: null,
+                    targetSVG: "M9,0C4.029,0,0,4.029,0,9s4.029,9,9,9s9-4.029,9-9S13.971,0,9,0z M9,15.93 c-3.83,0-6.93-3.1-6.93-6.93S5.17,2.07,9,2.07s6.93,3.1,6.93,6.93S12.83,15.93,9,15.93 M12.5,9c0,1.933-1.567,3.5-3.5,3.5S5.5,10.933,5.5,9S7.067,5.5,9,5.5 S12.5,7.067,12.5,9z",
                     socketConnection: null,
-                    person: {},
+                    keyword: null,
+                    tweets: [],
                     location: {},
                     organization: {},
+                    person: {},
                     tweetslocation: [],
                     others: {}
                 }
             }
 
             this.props.newChartDataListener(payload);
+            this.props.newMapDataListener(payload);
             this.props.actions.reset_data(payload);
-        }
+            this.props.newTweetPanelListener(payload);
+
+        
 
 
         let stompClient = null;
