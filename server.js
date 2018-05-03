4c7d6ae7a60729cd6f060941580ca90a6d1e212e
onur.iltan <onur.iltan@altpro.com.tr>
2018-05-03 13:22:56 +0300

    homepage deleted

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/twitteranalyzer/api/ping', function (req, res) {
   return res.send('pong');
 });
 
-app.get('/', function (req, res) {
+app.get('/twitteranalyzer', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
